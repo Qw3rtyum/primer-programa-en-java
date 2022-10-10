483fad019c5e48c9e46330715b41cb162fcd1d50
Qw3rtyum <pablolylm@gmail.com>
2022-10-10 09:57:24 +0200

    Borrado apellidos de Pablo y mostar banda favorita de Pablo

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("López y Laso Martínez");
 		System.out.println("22 años 2000");
 		System.out.println("Patatas fritas");
+		System.out.println("Iron Maiden");
 
 	}
 }
